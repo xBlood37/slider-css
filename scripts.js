19bda0793029591b1aa9f1b41cd2b671b75151c5
xBlood37 <123d.123b@mail.ru>
2023-03-02 19:43:27 +0300

    js show all

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const moreButton = document.querySelector('.more');
+const moreSpan = document.querySelector('.more__span');
+const slide9 = document.querySelector('#slide-9');
+const slide10 = document.querySelector('#slide-10');
+const slide11 = document.querySelector('#slide-11');
+
+console.log(slide9, slide10, slide11);
+
+moreButton.addEventListener('click', () => {
+  if (moreSpan.textContent === 'Скрыть') {
+    moreSpan.textContent = 'Показать все';
+    slide9.classList.remove('card--open-more');
+    slide10.classList.remove('card--open-more');
+    slide11.classList.remove('card--open-more');
+  } else if (moreSpan.textContent === 'Показать все') {
+    moreSpan.textContent = 'Скрыть';
+    slide9.classList.add('card--open-more');
+    slide10.classList.add('card--open-more');
+    slide11.classList.add('card--open-more');
+  }
+});
